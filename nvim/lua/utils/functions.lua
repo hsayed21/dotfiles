36a3d1efdaf25003cf378aeec08cc92686e8aecb
hsayed21 <hsayedhamed21@gmail.com>
2024-11-22 18:19:02 +0200

    🔧 Some Changes

--- nvim/lua/utils/functions.lua
+++ nvim/lua/utils/functions.lua
@@ -377,4 +377,31 @@ local function copy_git_relative()
 end
 
 
+function nvim_feedkeys(keys)
+    local feedable_keys = vim.api.nvim_replace_termcodes(keys, true, false, true)
+    vim.api.nvim_feedkeys(feedable_keys, "n", false)
+end
 
+function call(arg)
+    nvim_feedkeys(string.format(':call %s<enter>', arg))
+end
+
+function vsc_notify(arg)
+    call(string.format('VSCodeNotify("%s")', arg))
+end
+
+function vsc_call(arg)
+    call(string.format('VSCodeCall("%s")', arg))
+end
+
+function register_jump()
+    vsc_call('jumplist.registerJump')
+end
+
+function jump_back()
+    vsc_notify('jumplist.jumpBack')
+end
+
+function jump_forw()
+    vsc_notify('jumplist.jumpForward')
+end
