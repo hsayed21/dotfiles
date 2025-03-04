0c3029912a0c090b5548a81662cabdaff1fe7f23
hsayed21 <hsayedhamed21@gmail.com>
2025-03-04 19:24:57 +0200

    🔧 Some Changes

--- nvim/lua/plugins/editing.lua
+++ nvim/lua/plugins/editing.lua
@@ -235,16 +235,22 @@ return {
 	},
 	-- Escape key enhancement
 	{
-		"max397574/better-escape.nvim",
-		config = function()
-			require("better_escape").setup({
-				mapping = { "jk", "jj" }, -- a table with mappings to use
-				timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
-				clear_empty_lines = false, -- clear line after escaping if there is only whitespace
-				keys = "<Esc>",         -- keys used for escaping, if it is a function will use the result everytime
-			})
-		end,
-		enabled = not vim.g.vscode
+    "max397574/better-escape.nvim",
+    config = function()
+        require("better_escape").setup({
+            keys = function()
+                vim.api.nvim_input("<Esc>")
+                local current_line = vim.api.nvim_get_current_line()
+                if current_line:match("^%s+j$") then
+                    vim.schedule(function()
+                        vim.api.nvim_set_current_line("")
+                    end)
+                end
+            end,
+            timeout = vim.o.timeoutlen, -- Keeps using timeoutlen for timing
+        })
+    end,
+    enabled = not vim.g.vscode
 	},
 	-- Indentation
 	{
@@ -722,7 +728,7 @@ return {
 			},
 			{
 				mode = { "o", "x" },
-				"ie",
+				"iE",
 				function()
 					require("various-textobjs").entireBuffer()
 				end,
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -649,7 +649,7 @@ return {
 
 			local function lspClients()
 				local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-				local clients = vim.lsp.get_active_clients()
+				local clients = vim.lsp.get_clients()
 				if next(clients) == nil then
 					return ''
 				end
