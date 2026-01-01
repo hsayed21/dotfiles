8e889a74560626ae065e2e8bf5483a687840558f
hsayed21 <hsayedhamed21@gmail.com>
2026-01-02 00:25:12 +0200

    🔧 Some Changes

--- WindowsTerminal/Microsoft.PowerShell_profile.ps1
+++ WindowsTerminal/Microsoft.PowerShell_profile.ps1
@@ -103,7 +103,4 @@ try {
 }
 
 Set-PSReadLineKeyHandler -Chord "Tab" -Function "MenuComplete";
-#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
 
-Import-Module -Name Microsoft.WinGet.CommandNotFound
-#f45873b3-b655-43a6-b217-97c00aa0db58
